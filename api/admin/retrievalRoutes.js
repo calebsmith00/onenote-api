4a349afc26436701f2d33927ce3d34e7af874553
minifiedcode <minifiedcode@gmail.com>
2022-05-17 11:19:21 -0500

    Thanks format on save >:(

--- api/admin/retrievalRoutes.js
+++ api/admin/retrievalRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router()
 router.get('/retrieve-users', (req, res) => {
     getUsers()
         .then(response => res.json(response.value))
-        .catch(err => res.json({error: JSON.stringify(err)}))
+        .catch(err => res.json({ error: JSON.stringify(err) }))
 })
 
 export default router
